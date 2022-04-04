a0be2d8b702cef356a71d35b18254312a79393f5
Bayu Ahmad Fauzan <bayu.ahmad.f@mail.ugm.ac.id>
2022-04-04 18:03:16 +0700

    update js

--- script.js
+++ script.js
@@ -1,13 +1,127 @@
-const calculatorScreen = document.querySelector(".calculator-screen");
+// element html
+const numbers = document.querySelectorAll('.number');
+const wholeCalc = document.querySelector('.calculator');
+const calcScreen = document.querySelector('.calculator-screen');
+const operators = document.querySelectorAll('.operator');
+const equalSign = document.querySelector('.equal-sign');
+const clearBtn = document.querySelector('.all-clear');
+const decimal = document.querySelector('.decimal');
+const percen = document.querySelector('.percentage');
+const reset = document.querySelector('.reset');
 
+// variabel
+let prevNumber = '';
+let calcOperation = '';
+let currentNum = '0';
+
+// funtion
 const updateScreen = (number) => {
-  calculatorScreen.value = number;
+	calcScreen.value = number;
 };
 
-const numbers = document.querySelectorAll(".number");
+const inputNumber = (number) => {
+	if (currentNum === '0') {
+		currentNum = number;
+	} else {
+		currentNum += number;
+	}
+};
 
+const inputOperator = (operator) => {
+	if (calcOperation === '') {
+		prevNumber = currentNum;
+	}
+	calcOperation = operator;
+	currentNum = '0';
+};
+
+const calculate = () => {
+	let result = '';
+	switch (calcOperation) {
+		case '+':
+			result = parseFloat(prevNumber) + parseFloat(currentNum);
+			break;
+		case '-':
+			result = parseFloat(prevNumber) - parseFloat(currentNum);
+			break;
+		case '*':
+			result = parseFloat(prevNumber) * parseFloat(currentNum);
+			break;
+		case '/':
+			result = parseFloat(prevNumber) / parseFloat(currentNum);
+			break;
+		default:
+			break;
+	}
+
+	if (!isFinite(result)) {
+		if (isNaN(result)) {
+			// If result is not a number; set off by, eg, double-clicking operators
+			result = 'You broke it!';
+		} else {
+			// If result is infinity, set off by dividing by zero
+			result = 'OOPS!!';
+			wholeCalc.classList.add('broken'); // Break calculator
+			reset.classList.remove('hide'); // And show reset button
+			reset.classList.add('show'); // And show reset button
+		}
+	}
+
+	currentNum = result;
+	calcOperation = '';
+};
+
+const allClear = () => {
+	prevNumber = '';
+	calcOperation = '';
+	currentNum = '0';
+};
+
+const inputDecimal = (dot) => {
+	if (currentNum.includes('.')) {
+		return;
+	}
+	currentNum += dot;
+};
+
+// eventListener
 numbers.forEach((number) => {
-  number.addEventListener("click", (event) => {
-    console.log(event.target.value);
-  });
+	number.addEventListener('click', (event) => {
+		inputNumber(event.target.value);
+		updateScreen(currentNum);
+	});
+});
+
+operators.forEach((operator) => {
+	operator.addEventListener('click', (event) => {
+		inputOperator(event.target.value);
+	});
+});
+
+equalSign.addEventListener('click', () => {
+	calculate();
+	updateScreen(currentNum);
+});
+
+clearBtn.addEventListener('click', () => {
+	allClear();
+	updateScreen(currentNum);
+});
+
+decimal.addEventListener('click', (event) => {
+	inputDecimal(event.target.value);
+	updateScreen(currentNum);
+});
+
+percen.addEventListener('click', () => {
+	currentNum = parseFloat(currentNum) / 100;
+	updateScreen(currentNum);
+});
+
+reset.addEventListener('click', () => {
+	wholeCalc.classList.remove('broken');
+	reset.classList.remove('show');
+	reset.classList.add('hide');
+	allClear();
+	updateScreen(currentNum);
 });
